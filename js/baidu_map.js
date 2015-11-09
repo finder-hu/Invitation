603b811b66ae1defd90b111c4cd630a27fb7448f
Finder hu <1397812937@qq.com>
2015-11-09 18:49:55 +0800

    sdf

--- js/baidu_map.js
+++ js/baidu_map.js
@@ -5,9 +5,7 @@ map.centerAndZoom(new BMap.Point(103.8846250000, 30.8396420000), 11);  // 初始
 map.addControl(new BMap.MapTypeControl());   //添加地图类型控件
 map.setCurrentCity("成都");          // 设置地图显示的城市 此项是必须设置的
 map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
-//加标记
-var marker = new BMap.Marker(point);
-map.addOverlay(marker);
-marker.addEventListener("click",function(){
-	map.openInfoWindow(infoWindow,new BMap.Point(103.8846250000,30.8396420000));
-})
+
+var point = new BMap.Point(103.9069630000, 30.8140170000);      
+var marker = new BMap.Marker(point);        // 创建标注    
+map.addOverlay(marker);                     // 将标注添加到地图中
